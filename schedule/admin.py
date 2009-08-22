31fe7976c6717e13bee76c0a4ef0bf153aa5b829
Henning Hraban Ramm <hraban@fiee.net>
2009-08-22 10:31:20 +0200

    added EventAdminOptions to admin.py

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-
 from schedule.models import Calendar, Event, CalendarRelation, Rule
 
 class CalendarAdminOptions(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ['name']
+    list_display = ('name', 'slug')
 
+class EventAdminOptions(admin.ModelAdmin):
+    list_display = ('start', 'end', 'title', 'calendar', 'creator')
+    list_display_links = ('start', 'title')
+    search_fields = ('title', 'description')
+    list_filter = ('start','calendar','creator')
+    radio_fields = {'calendar':admin.HORIZONTAL, 'rule': admin.HORIZONTAL}
 
 admin.site.register(Calendar, CalendarAdminOptions)
-admin.site.register([Rule, Event, CalendarRelation])
+admin.site.register(Event, EventAdminOptions)
+admin.site.register([Rule, CalendarRelation])
